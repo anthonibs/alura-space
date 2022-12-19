321abcb0b20ce688e2c13d68f6a01e497c4f8e79
Anthoni Broering dos Santos <anthoniibs@gmail.com>
2022-12-19 20:09:09 -0300

    init:  🎉  inicialização projeto alura space

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./pages/Home";
+import "./index.scss"
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../components/Cabecalho";
+import Menu from "../components/Menu";
+
+export default function Home() {
+  return (
+    <>
+      <Cabecalho />
+      <Menu />
+    </>
+  )
+}
